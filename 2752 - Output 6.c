499ab4af682bc3e177e4c809ed8484ddcaf03211
Raiyan Jahan <rynrafsan@gmail.com>
2026-02-22 21:48:22 +0600

    Add files via upload

---
+++ 2752 - Output 6.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[50]={"AMO FAZER EXERCICIO NO URI"};
+   printf("<% s>\n",s);
+   printf("<% 30s>\n",s);
+   printf("<% .20s>\n",s);
+   printf("<% -20s>\n",s);
+   printf("<% -30s>\n",s);
+   printf("<% .30s>\n",s);
+   printf("<% 30.20s>\n",s);
+   printf("<% -30.20s>\n",s);
+   return 0;
+}
